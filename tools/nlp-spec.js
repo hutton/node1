1feaef54cebbe6517017fe199cdd86318ec02045
Simon <simon.hutton@gmail.com>
2013-06-10 21:48:08 +0100

    Started nlp

--- tools/nlp-spec.js
+++ tools/nlp-spec.js
@@ -1,7 +1,94 @@
-var nlp = require("nlp");
+var nlp = require("./nlp");
 var moment = require("moment");
 var sugar = require("sugar");
+var _ = require("underscore");
 
-var context = {today: new Date("April 9, 2013")};
 
-nlp.extractDates("25th would be OK", context);
+function compareMatches(expected, result){
+	if (expected.length !== result.length){
+		return false;
+	}
+ 
+	for (var i=0; i < expected.length; i++){
+		if (!expected[i].date.is(result[i].date) || expected[i].index !== result[i].index || expected[i].match !== result[i].match ){
+			return false;
+		}
+	}
+ 
+	return true;
+}
+
+function validate(expected, text){
+	var result = nlp.extractDates(text);
+
+	if (compareMatches(expected, result)){
+		console.log( "Success with: " + text);
+	} else {
+		console.log( "*********************");
+		console.log( "Failed! with: " + text);
+		console.log( "Expected:");
+		prettyPrintResult(expected);
+		console.log( "Result:");
+		prettyPrintResult(result);
+		console.log( "*********************");
+	}
+}
+
+function prettyPrintResult(result){
+	for (var i=0; i < result.length; i++){
+		console.log("match: " + result[i].match + " date: " + result[i].date + " index: " + result[i].index);
+	}
+}
+
+console.log(nlp.processBody2("3rd match 18ee not dates 19th end"));
+
+
+// return nlp.addSentimentToMatches("3rd match 18ee not dates 19th end", [
+// 	{ match: "3rd", date: Date.future("3rd"), index: 0 },
+// 	{ match: "19th", date: Date.future("19th"), index: 25 },
+// 	]);
+
+// Simple dates e.g. 1st, 22nd 
+// validate([
+// 	{ match: "21st", date: Date.future("21st"), index: 0 },
+// 	], "21st match");
+// validate([
+// 	{ match: "1st", date: Date.future("1st"), index: 0 },
+// 	{ match: "18th", date: Date.future("18th"), index: 10 },
+// 	{ match: "22nd", date: Date.future("22nd"), index: 28 },
+// 	], "1st match 18th not 47 dates 22nd");
+// validate([
+// 	{ match: "3rd", date: Date.future("3rd"), index: 0 },
+// 	{ match: "19th", date: Date.future("19th"), index: 25 },
+// 	], "3rd match 18ee not dates 19th end");
+
+// // Simple day e.g. Wednesday, thurs, fri
+// validate([
+// 	{ match: "Friday", date: Date.future("Friday"), index: 5 },
+// 	], "next Friday is good");
+// validate([
+// 	{ match: "thurs", date: Date.future("Thursday"), index: 0 },
+// 	], "thurs is good");
+// validate([
+// 	{ match: "thurs", date: Date.future("Thursday"), index: 0 },
+// 	{ match: "Wednesday", date: Date.future("Wednesday"), index: 9 },
+// 	{ match: "Saturday", date: Date.future("Saturday"), index: 22 },
+// 	{ match: "sunday", date: Date.future("Sunday"), index: 40 },
+// 	], "thurs is Wednesday or Saturday good not sunday");
+
+// // Two part dates e.g. Friday 21st
+// validate([
+// 	{ match: "Friday 21st", date: Date.future("21st"), index: 0 },
+// 	], "Friday 21st is good");
+// validate([
+// 	{ match: "Friday 21st", date: Date.future("21st"), index: 0 },
+// 	{ match: "Mon 1st", date: Date.future("1st"), index: 33 },
+// 	{ match: "22nd", date: Date.future("22nd"), index: 20 },
+// 	], "Friday 21st and the 22nd but not Mon 1st");
+// validate([
+// 	{ match: "Friday 21st", date: Date.future("21st"), index: 0 },
+// 	{ match: "Saturday 2nd", date: Date.future("2nd"), index: 12 },
+// 	{ match: "Wed 3rd", date: Date.future("3rd"), index: 25 },
+// 	{ match: "Thursday", date: Date.future("Thursday"), index: 38 },
+// 	], "Friday 21st Saturday 2nd,Wed 3rd then Thursday");
+
--- tools/nlp.js
+++ tools/nlp.js
@@ -2,6 +2,7 @@ var _ = require("underscore");
 
 var natural = require("natural");
 var moment = require("moment");
+var sugar = require("sugar");
 var logger = require("../tools/logger");
 
 var classifier = new natural.BayesClassifier();
@@ -15,13 +16,248 @@ function train(){
 	classifier.addDocument("i'm not free on any dddd.", 'busy');
 	classifier.addDocument("i can only do dddd", 'free');
 
+	classifier.addDocument("would be OK", 'free');
+
 	classifier.train();
 }
 
 train();
 
-function processBody2(calendar, body){
-	classifier.classify(body);
+function blankMatches(text, matches){
+	if (matches.length == 0){
+		return text;
+	}
+
+	var chars = text.chars();
+
+	var string = "";
+	var matchIndex = 0;
+
+	var match = matches[matchIndex];
+	var finished = false;
+
+	for (var charIndex=0; charIndex < chars.length; charIndex++){
+		if (!finished && charIndex >= match.index){
+			string += "_";
+
+			if (charIndex >= (match.index + match.match.length - 1)){					
+				matchIndex++;
+
+				if (matchIndex > matches.length - 1){
+					finished = true;
+				}
+
+				match = matches[matchIndex];
+			}
+		} else {
+			string += chars[charIndex];
+		}
+	}
+
+	return string;
+}
+
+// Simple dates e.g. 1st, 22nd 
+function simpleDate(text){
+  var re = /[0-9]{1,2}(th|rd|st|nd)/g
+ 
+	var result;
+	var matches = [];
+ 
+	while((result = re.exec(text)) !== null){
+		var match = {
+			match: result[0],
+			date: Date.future(result[0]),
+			index: result.index
+		};
+ 
+		matches.push(match);
+	}
+ 
+	return matches;
+}
+
+function formatDay(day){
+	day = day.replace(/mon( |$)/ig,'monday');
+	day = day.replace(/tues( |$)/ig,'tuesday');
+	day = day.replace(/wed( |$)/ig,'wednesday');
+	day = day.replace(/thurs( |$)/ig,'thursday');
+	day = day.replace(/fri( |$)/ig,'friday');
+
+	return day;
+}
+
+// Simple day e.g. Wednesday, thurs, fri
+function simpleDay(text){
+  var re = /(monday|tuesday|wednesday|thursday|friday|saturday|sunday|mon|tues|wed|thurs|fri)/ig
+ 
+	var result;
+	var matches = [];
+ 
+	while((result = re.exec(text)) !== null){
+
+		var formattedDay = formatDay(result[0]);
+
+		var match = {
+			match: result[0],
+			date: Date.future(formattedDay),
+			index: result.index
+		};
+ 
+		matches.push(match);
+	}
+ 
+	return matches;
+}
+
+function twoPartDate(text){
+  var re = /(monday|tuesday|wednesday|thursday|friday|saturday|sunday|mon|tues|wed|thurs|fri) [0-9]{1,2}(th|rd|st|nd|)/ig
+ 
+	var result;
+	var matches = [];
+ 
+	while((result = re.exec(text)) !== null){
+
+		var datePartMatch = result[0].match("[0-9]{1,2}(th|rd|st|nd)");
+
+		var match = {
+			match: result[0],
+			date: Date.future(datePartMatch[0]),
+			index: result.index
+		};
+
+		matches.push(match);
+	}
+
+	return matches;
+}
+
+function extractDates(text){
+	var matches = [];
+
+	matches.push.apply(matches, twoPartDate(text));
+	text = blankMatches(text, matches);
+	matches.push.apply(matches, simpleDate(text));
+	text = blankMatches(text, matches);
+	matches.push.apply(matches, simpleDay(text));
+	text = blankMatches(text, matches);
+
+	return matches;
+}
+
+function getSentiment(text){
+	return classifier.classify(text);
+}
+
+function addSentimentToMatches(text, matches){
+	if (matches.length == 0){
+		return matches;
+	}
+
+	var matchesWithSentiment = [];
+
+	var splitStart = 0;
+	var matchIndex = 0;
+	var match = matches[matchIndex];
+
+	while (splitStart < text.length){
+		var sentimentText = text.slice(splitStart, match.index);
+		var sentiment = getSentiment(sentimentText);
+
+		if (sentimentText.length > 1){
+			matchesWithSentiment.push({match: sentimentText, sentiment: sentiment, index: splitStart});
+		}
+
+		matchesWithSentiment.push(match);
+
+		splitStart = match.index + match.match.length;
+
+		matchIndex++;
+
+		if (matchIndex >= matches.length){
+			var sentimentText = text.slice(splitStart, text.length);
+			var sentiment = getSentiment(sentimentText);
+
+			if (sentimentText.length > 1){
+				matchesWithSentiment.push({match: sentimentText, sentiment: sentiment, index: splitStart});	
+			}
+
+			break;
+		}
+
+		match = matches[matchIndex];
+	}
+
+	return matchesWithSentiment;
+}
+
+function sentenceParser(text) {
+    var tmp = text.split(/(\S.+?[.])(?=\s+|$)/g);
+    var sentences = [];
+    //join acronyms, titles
+    for (var i in tmp) {
+        if (tmp[i]) {
+            tmp[i] = tmp[i].replace(/^\s+|\s+$/g, ''); //trim extra whitespace
+            //join common abbreviations + acronyms
+            if (tmp[i].match(/(^| )(mr|dr|llb|md|bl|phd|ma|ba|mrs|miss|misses|mister|sir|esq|mstr|jr|sr|st|lit|inc|fl|ex|eg|jan|feb|mar|apr|jun|aug|sept?|oct|nov|dec)\. ?$/i) || tmp[i].match(/[ |\.][a-z]\.?$/i)) {
+                tmp[parseInt(i) + 1] = tmp[i] + ' ' + tmp[parseInt(i) + 1];
+            }
+            else {
+                sentences.push(tmp[i]);
+                tmp[i] = '';
+            }
+        }
+    }   
+    //cleanup afterwards
+    var clean = [];
+    for (var i in sentences) {
+        sentences[i] = sentences[i].replace(/^\s+|\s+$/g, ''); //trim extra whitespace
+        if (sentences[i]) {
+            clean.push(sentences[i]);
+        }
+    }
+    return clean;
+}
+
+function turnMatchesIntoDates(matches){
+	var busyDates = [];
+	var freeDates = [];
+
+	var currentSentiment = freeDates;
+
+	matches.each(function(match){
+		if (!_.isUndefined(match.date)){
+			currentSentiment.push(match.date);
+		} else if (!_.isUndefined(match.sentiment)){
+			if (match.sentiment === "free"){
+				currentSentiment = freeDates;
+			} else if (match.sentiment === "busy"){
+				currentSentiment = busyDates;
+			}
+		}
+	});
+
+	return [busyDates, freeDates];
+}
+
+function processBody2(body){
+	var sentences = sentenceParser(body);
+
+	var busyDates = [];
+	var freeDates = [];
+
+	sentences.each(function(sentence){
+		var matches = extractDates(body);
+
+		matches = addSentimentToMatches(body, matches);
+
+		dates = turnMatchesIntoDates(matches);
+
+		busyDates.push(dates[0]);
+		freeDates.push(dates[1]);
+	});
+
+	return [busyDates, freeDates];
 }
 
 function processBody(calendar, body){
@@ -41,5 +277,10 @@ function processBody(calendar, body){
 }
 
 module.exports = {
-	processBody: processBody
+	processBody: processBody,
+	processBody2: processBody2,
+	extractDates: extractDates,
+	blankMatches: blankMatches,
+	addSentimentToMatches: addSentimentToMatches
+
 }
