e9ed4ac57106790f90fde8a335c067880a657604
hutton <simon.hutton@gmail.com>
2013-11-06 11:43:58 +0000

    Update mail.js

--- routes/mail.js
+++ routes/mail.js
@@ -174,7 +174,7 @@ exports.sendGridReceive = function(req, res){
 exports.receive = function(req, res){
 	logger.info("Mail received from browser");
 	
-	logger.info(req.body);
+	logger.info(req);
 
 	processEmailRequest(req, res, function(newCalendar){
 		res.redirect('/calendar/' + newCalendar.id);
