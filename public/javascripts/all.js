75a4e3abd0a8535c52fd110482a3e27edd34815c
Simon <simon.hutton@gmail.com>
2013-11-20 18:20:18 +0000

    Minimised

---
+++ public/javascripts/all.js
@@ -0,0 +1,24 @@
+
+
+
+// @codekit-prepend "jquery.js";
+
+// @codekit-prepend "jquery-1.8.2.min.js";
+  
+// @codekit-prepend "bootstrap/bootstrap.min.js";
+// @codekit-prepend "fastclick.js";
+
+// @codekit-prepend "underscore-min.js";
+// @codekit-prepend "moment.js";
+// @codekit-prepend "backbone-min.js";
+
+// @codekit-prepend "helper.js";
+  
+// @codekit-prepend "models/choice-model.js";
+// @codekit-prepend "models/choices-model.js";
+// @codekit-prepend "models/calendar-model.js";
+
+// @codekit-prepend "views/choices-view.js";
+
+// @codekit-prepend "event-app.js";
+
