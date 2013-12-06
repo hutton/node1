d3c3f1457b5b91ca4707380b53fa1f09f396762d
Simon <simon.hutton@gmail.com>
2013-12-06 21:01:29 +0000

    Added from_name

--- tools/mail.js
+++ tools/mail.js
@@ -212,6 +212,7 @@ function sendMailToAttendee(calendar, toAttendee, subject, message, fromName){
 						"text": text,
 						"html": html,
 						"subject": subject,
+						"from_name": subject + " - Convenely",
 						"from_email": calendar.id + "@convenely.com",
 						"to": [{
 							"email": toAttendee.email,
